e7e62225ee3712532f38f6c01ef6dae6b423d542
dev-deku <nbnloer@gmail.com>
2023-06-21 00:27:08 +0900

    Create React Porject

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+        <img src={logo} lat="logo"/>
+        Let`s develop managerment service; 
+    </div>
+  );
+}
+
+export default App;
